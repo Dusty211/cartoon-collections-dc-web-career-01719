b7fa29e3e978aa8dc023b6b58d48462416dd896f
Kyle Houghton <kylebenz@hotmail.com>
2018-12-24 19:13:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ foods = %w[butter cheddar gouda]
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.each {|item| return item if cheese_types.include? item}
+  foods.find {|item| cheese_types.include? item}
 end
 
 find_the_cheese(foods)
