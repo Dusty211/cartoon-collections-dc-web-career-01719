365cd26429c4f2d52c86ab47c359e0bf06563e21
Kyle Houghton <kylebenz@hotmail.com>
2018-12-24 18:22:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,8 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? {|call| call.length > 4}
 end
 
+foods = %w[butter lettuce computer]
+
 def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
