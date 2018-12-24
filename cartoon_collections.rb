9ba3eb7e36ed8bc711866f2a54d2af02531b284f
Kyle Houghton <kylebenz@hotmail.com>
2018-12-24 18:27:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ foods = %w[butter lettuce gouda]
 def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  puts cheese_types.collect {|cheese| foods.include? cheese}
+  puts cheese_types.collect {|cheese| foods.include? cheese}.include? false
 end
 
 find_the_cheese(foods)
